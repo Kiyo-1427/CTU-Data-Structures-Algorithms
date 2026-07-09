200852b4cf1423eedfa848097604877cc9ea44f8
Kiyo <haob2509724@student.ctu.edu.vn>
2026-07-09 18:42:46 +0700

    them ghi chu

--- Danh_Sach_Dac/phep_toan_so_nguyen/cau_2_khai_bao_list.cpp
+++ Danh_Sach_Dac/phep_toan_so_nguyen/cau_2_khai_bao_list.cpp
@@ -1,6 +1,9 @@
+//khai báo độ dài mảng
 #define Maxlength 100
+//đổi tên thằng int cho sang mồm
 typedef int ElementType;
 typedef int Position;
+//tạo struct list
 typedef struct{
     ElementType Elements[Maxlength];
     Position Last;
